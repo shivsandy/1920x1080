8d6d6a58522ee14d403b4146fe8e1d4fc3729d36
shiva8yte <71714299+shivsandy@users.noreply.github.com>
2025-03-26 04:37:41 -0700

    Update uninstaller.bat

--- uninstaller.bat
+++ uninstaller.bat
@@ -2,7 +2,6 @@
 :: Request admin privileges
 net session >nul 2>&1
 if %errorLevel% neq 0 (
-    echo Running with administrator privileges...
     powershell -Command "Start-Process cmd -ArgumentList '/c %~s0' -Verb RunAs"
     exit
 )
@@ -11,23 +10,15 @@ if %errorLevel% neq 0 (
 set "shortcutPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\resolution.lnk"
 if exist "%shortcutPath%" (
     del "%shortcutPath%"
-    echo Deleted startup shortcut.
-) else (
-    echo Startup shortcut not found.
 )
 
 :: Step 2: Delete the folder in Program Files
 set "folderPath=C:\Program Files\FixResolution"
 if exist "%folderPath%" (
     rmdir /s /q "%folderPath%"
-    echo Deleted FixResolution folder.
-) else (
-    echo FixResolution folder not found.
 )
 
 :: Step 3: Delete the Task Scheduler job
-schtasks /delete /tn "FixResolutionTask" /f
-echo Deleted Task Scheduler job.
+schtasks /delete /tn "FixResolutionTask" /f >nul 2>&1
 
-echo Cleanup completed!
-pause
+exit
